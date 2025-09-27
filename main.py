be8f0c7284b0b70aa74bec5006fdaccaf66d1d8b
JordanSkousen <jotheman21@gmail.com>
2025-09-27 12:03:55 -0600

    Remove filter fps 30

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def trim_video(video, out_file, start_frame, end_frame, encode_mp4):
   start = frame_to_timestamp(start_frame, video.fps)
   end = frame_to_timestamp(end_frame, video.fps)
   print("Trimming video \"{}\" from timestamps {} to {} and saving to \"{}\"...".format(video.file, start, end, out_file))
-  args = ["ffmpeg", "-y", "-ss", start, "-i", video.file, "-to", end, "-filter:v", "fps=30"]
+  args = ["ffmpeg", "-y", "-ss", start, "-i", video.file, "-to", end]
   if encode_mp4:
     args.extend(["-c", "copy"])
   args.append(out_file)
